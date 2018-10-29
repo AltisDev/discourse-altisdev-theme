388d9ce54f1b0f88cbc6dafce0afc06468c341f8
Tony Briet <tony@octree.ch>
2018-10-29 02:34:57 +0100

    fix: consistency between all lists

--- src/common/plugin.js
+++ src/common/plugin.js
@@ -7,3 +7,23 @@ api.reopenWidget('header-buttons', {
                                    icon: 'user' })
   }
 })
+
+Discourse._registerPluginCode('0.8.9', function (api) {
+  api.decorateWidget('header-icons:before', function (helper) {
+      return helper.h('li.dropdown#chat', [helper.h('button.dropdown-toggle#chat-button.icon', {
+          'attributes': {
+              "type": "button",
+              "data-toggle": "dropdown",
+              "title": "discord chat"
+          } }, helper.h('i.fa.fa-comments-o.chat-button-icon.d-icon')), helper.h('ul.dropdown-menu#chat', [helper.h('li', [helper.h('iframe', {
+          "src": 'https://discordapp.com/widget?id=342041941174910986&theme=light', 
+          "width": "350",
+          "height": "500",
+          "allowtransparency": "true",
+          "frameborder": "0",
+          "id": "chatwidget",
+          "name": "chatwidget"
+          
+      })])])]);
+  });
+});
